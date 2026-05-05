6cfa610235418b138f054003297d6797d5715fba
houstonhaynes <8174976+houstonhaynes@users.noreply.github.com>
2026-05-05 18:14:09 -0400

    regenerate for SyntheticPathAssignment walk

--- generators/xantham/output/dynamic-workflows.fs
+++ generators/xantham/output/dynamic-workflows.fs
@@ -1,6 +1,9 @@
 module rec Cloudflare =
     module rec DynamicWorkflows =
         module rec DispatchWorkflow =
+            type _Lit3 =
+                abstract Item: key: string -> option<obj>
+
             type _Lit1 =
                 abstract ctx: option<obj> with get, set
                 abstract env: 'Env with get, set
@@ -10,35 +13,33 @@ module rec Cloudflare =
 
         module rec Dist =
             module rec Binding =
-                [<Import("./binding.js", "WrapWorkflowBindingOptions")>]
-                type WrapWorkflowBindingOptions =
-                    abstract bindingName: option<string> with get, set
-
                 [<Import("./binding.js", "DynamicWorkflowBindingProps")>]
                 type DynamicWorkflowBindingProps =
                     abstract metadata: Types.DispatcherMetadata with get, set
                     abstract bindingName: string with get, set
 
+                [<Import("./binding.js", "WrapWorkflowBindingOptions")>]
+                type WrapWorkflowBindingOptions =
+                    abstract bindingName: option<string> with get, set
+
             module rec Types =
+                type DispatcherMetadata = obj
+
                 [<Import("./types.js", "WorkflowEventLike")>]
                 type WorkflowEventLike<'T> =
                     abstract instanceId: string with get, set
                     abstract timestamp: Date with get, set
                     abstract payload: 'T with get, set
 
-                type DispatcherMetadata = obj
-
         module rec LoadWorkflowRunner =
             type Invoke =
                 abstract run: event: Dist.Types.WorkflowEventLike<'T> * step: obj -> Promise<'R>
 
-        module rec LoadWorkflowRunnerContext =
-            type _Lit1 =
-                abstract Item: key: string -> option<obj>
-
-        [<Import("@cloudflare/dynamic-workflows", "WorkflowRunner")>]
-        type WorkflowRunner<'R, 'T> =
-            abstract run: event: Dist.Types.WorkflowEventLike<'T> * step: obj -> Promise<'R>
+        [<Import("@cloudflare/dynamic-workflows", "LoadWorkflowRunner")>]
+        type LoadWorkflowRunner<'R, 'T, 'Env> =
+            abstract Invoke:
+                context: LoadWorkflowRunnerContext<'Env> ->
+                    U2<Promise<LoadWorkflowRunner.Invoke>, LoadWorkflowRunner.Invoke>
 
         [<Import("@cloudflare/dynamic-workflows", "MissingDispatcherMetadataError"); AbstractClass; AllowNullLiteral>]
         type MissingDispatcherMetadataError private () =
@@ -47,20 +48,23 @@ module rec Cloudflare =
             [<EmitConstructor>]
             abstract Create: unit -> MissingDispatcherMetadataError
 
-        type DynamicWorkflowBinding =
-            abstract Item: key: string -> option<obj>
+        [<Import("@cloudflare/dynamic-workflows", "DynamicWorkflowBinding"); AbstractClass; AllowNullLiteral>]
+        type DynamicWorkflowBinding private () =
+            inherit option<obj<DynamicWorkflowBinding, Dist.Binding.DynamicWorkflowBindingProps>>()
+            abstract impl: option<obj> with get, set
+            abstract get: id: string -> Promise<option<obj>>
+            abstract createBatch: batch: ResizeArray<option<obj>> -> Promise<ResizeArray<option<obj>>>
+            abstract create: ?options: obj -> Promise<option<obj>>
+
+        [<Import("@cloudflare/dynamic-workflows", "WorkflowRunner")>]
+        type WorkflowRunner<'R, 'T> =
+            abstract run: event: Dist.Types.WorkflowEventLike<'T> * step: obj -> Promise<'R>
 
         [<Import("@cloudflare/dynamic-workflows", "LoadWorkflowRunnerContext")>]
         type LoadWorkflowRunnerContext<'Env> =
             abstract ctx: option<obj> with get, set
             abstract env: 'Env with get, set
             abstract metadata: Dist.Types.DispatcherMetadata with get, set
-
-        [<Import("@cloudflare/dynamic-workflows", "LoadWorkflowRunner")>]
-        type LoadWorkflowRunner<'R, 'T, 'Env> =
-            abstract Invoke:
-                context: LoadWorkflowRunnerContext<'Env> ->
-                    U2<Promise<LoadWorkflowRunner.Invoke>, LoadWorkflowRunner.Invoke>
 
         type DispatchWorkflow =
             abstract Invoke:
@@ -79,6 +83,18 @@ module rec Cloudflare =
                 : option<obj> =
                 JS.undefined
 
+            [<Import("@cloudflare/dynamic-workflows", "createDynamicWorkflowEntrypoint")>]
+            static member createDynamicWorkflowEntrypoint
+                (
+                    loadRunner:
+                        LoadWorkflowRunnerContext<'Env>
+                            -> U2<
+                                CreateDynamicWorkflowEntrypoint.LoadRunner,
+                                Promise<CreateDynamicWorkflowEntrypoint.LoadRunner>
+                             >
+                ) : option<obj> =
+                JS.undefined
+
             [<Import("@cloudflare/dynamic-workflows", "wrapWorkflowBinding")>]
             static member wrapWorkflowBinding
                 (metadata: Dist.Types.DispatcherMetadata, ?options: Dist.Binding.WrapWorkflowBindingOptions)
@@ -95,16 +111,4 @@ module rec Cloudflare =
                         LoadWorkflowRunnerContext<'Env>
                             -> U2<DispatchWorkflow.LoadRunner, Promise<DispatchWorkflow.LoadRunner>>
                 ) : Promise<'Result> =
-                JS.undefined
-
-            [<Import("@cloudflare/dynamic-workflows", "createDynamicWorkflowEntrypoint")>]
-            static member createDynamicWorkflowEntrypoint
-                (
-                    loadRunner:
-                        LoadWorkflowRunnerContext<'Env>
-                            -> U2<
-                                CreateDynamicWorkflowEntrypoint.LoadRunner,
-                                Promise<CreateDynamicWorkflowEntrypoint.LoadRunner>
-                             >
-                ) : option<obj> =
                 JS.undefined
